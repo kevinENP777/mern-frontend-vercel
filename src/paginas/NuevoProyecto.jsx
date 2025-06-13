14e7ed78a2814ebf8e20aee2df67c53c8bbcafbf
kevin <kevin109235236@gmail.com>
2025-06-13 16:32:34 -0500

    error 304

--- src/paginas/NuevoProyecto.jsx
+++ src/paginas/NuevoProyecto.jsx
@@ -3,10 +3,12 @@ import FormularioProyecto from "../components/FormularioProyecto"
 const NuevoProyecto = () => {
   return (
     <>
-      <h1 className="text-4xl font-black text-black text-center mb-6">Crear Nueva Idea de Negocio</h1>
+      <h1 className="text-4xl font-black text-black text-center mb-6">
+        Crear Nueva Idea de Negocio
+      </h1>
 
-      <div className="mt-10 flex justify-center p-6 bg-[#ffffff] rounded-lg shadow-lg">
-        <div className="w-full max-w-2xl bg-white p-6 rounded-md shadow-md">
+      <div className="w-full flex justify-center">
+        <div className="mt-10 p-6 bg-[#ffffff] rounded-lg shadow-lg w-full max-w-2xl">
           <FormularioProyecto />
         </div>
       </div>
