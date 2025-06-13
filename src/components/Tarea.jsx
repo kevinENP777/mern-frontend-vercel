2b98de1d48f434b1eeaeb49e6b782abcd797e5f9
kevin <kevin109235236@gmail.com>
2025-06-13 11:50:27 -0500

    corre

--- src/components/Tarea.jsx
+++ src/components/Tarea.jsx
@@ -27,36 +27,39 @@ const Tarea = ({ tarea }) => {
                 <p className="mb-1 text-gray-600">Prioridad: {prioridad}</p>
             </div>
             <div className="flex gap-2">
-                <button
-                    className="bg-[#36681d] hover:bg-[#203c12] text-white font-bold py-2 px-4 rounded transition-colors"
-                    onClick={() => handleModalEditarTarea(tarea)}
-                >
-                    Editar
-                </button>
-                {estado ? (
-                    <button className="bg-sky-600 px-4 py-3 text-white uppercase font-bold text-sm rounded-lg">
-                        Completa
-                    </button>
-                ) : (
-                   <button 
-                        className="bg-[#39A900] hover:bg-[#2F8A00] text-white font-bold py-2 px-4 rounded transition-colors"
-                        onClick={() => {
-                            console.log("Enviando tarea a Admin:", tarea);
-                            enviarTarea(tarea);
-                            alert("Enviado con éxito"); // Alerta añadida aquí
-                        }}
-                    >
-                        ENVIAR
-                    </button>
+               <div className="flex gap-2">
+                        <button
+                            className="bg-[#36681d] hover:bg-[#203c12] text-white font-bold py-2 px-4 text-sm rounded-lg transition-colors"
+                            onClick={() => handleModalEditarTarea(tarea)}
+                        >
+                            EDITAR
+                        </button>
 
+                        {estado ? (
+                            <button className="bg-sky-600 px-4 py-2 text-white uppercase font-bold text-sm rounded-lg">
+                                COMPLETA
+                            </button>
+                        ) : (
+                            <button 
+                                className="bg-[#39A900] hover:bg-[#2F8A00] text-white font-bold py-2 px-4 text-sm rounded-lg transition-colors"
+                                onClick={() => {
+                                    console.log("Enviando tarea a Admin:", tarea);
+                                    enviarTarea(tarea);
+                                    alert("Enviado con éxito");
+                                }}
+                            >
+                                ENVIAR
+                            </button>
+                        )}
 
-                )}
-                <button
-                    className="bg-red-600 px-4 py-3 text-white uppercase font-bold text-sm rounded-lg"
-                    onClick={() => handleModalEliminarTarea(tarea)}
-                >
-                    Eliminar
-                </button>
+                        <button
+                            className="bg-red-600 px-4 py-2 text-white uppercase font-bold text-sm rounded-lg"
+                            onClick={() => handleModalEliminarTarea(tarea)}
+                        >
+                            ELIMINAR
+                        </button>
+                    </div>
+
             </div>
         </div>
     );
