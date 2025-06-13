d133ace8f2461bc41a707f7ac60946a5bfa63a2c
kevin <kevin109235236@gmail.com>
2025-06-13 11:44:19 -0500

    correccion199

--- src/paginas/ConfirmarCuenta.jsx
+++ src/paginas/ConfirmarCuenta.jsx
@@ -40,15 +40,16 @@ const ConfirmarCuenta = () => {
 
     return (
         <>
-            <h1 className="text-sky-600 font-black text-6xl capitalize">Confirma tu cuenta sena y crea tus {' '}
-                <span className="text-slate-700">Ideas de Negocio</span>
+            <h1 className="text-sky-600 font-black text-6xl capitalize">Confirma tu cuenta {' '}
+                <span className="text-slate-700">F.E.S</span>
             </h1>
             <div className="mt-20 md:mt-10 shadow-lg px-5 py-10 rounded-xl bg-white">
                 {msg && <Alerta alerta={alerta}/>}
 
                 {cuentaConfirmada && (
                 <Link
-                    className='block text-center my-5 🟦text-slate-500 uppercase text-sm'
+                    className='block text-center my-5 text-green-600 uppercase text-sm'
+
                     to='/'
                 >
                     Inicia Sesión
