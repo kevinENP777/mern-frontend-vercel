7c211bc81594b67c76f6023f990022d591279fab
kevinENP777 <kevin109235236@gmail.com>
2025-07-18 10:28:23 -0500

    Update Alerta.jsx

--- src/components/Alerta.jsx
+++ src/components/Alerta.jsx
@@ -1,11 +1,11 @@
-const Alerta = ({ alerta, pruebas }) => {
-  if (!alerta || !alerta.msg) return null;
-
+const Alerta = ({ alerta }) => {
   return (
-    <div className={`${alerta.error ? 'from-red-400 to-red-600' : 
+    <div className={${alerta.error ? 'from-red-400 to-red-600' : 
     'from-green-500 to-green-700'} bg-gradient-to-br text-center p-3 rounded-xl 
-    uppercase text-white font-bold text-sm my-10`}>
+    uppercase text-white font-bold text-sm my-10}>
       {alerta.msg}
     </div>
-  );
+  )
 }
+
+export default Alerta
